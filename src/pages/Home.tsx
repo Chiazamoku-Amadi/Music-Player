6363ab9bcbfe6e334525f0c75556ae372a3b90df
Chiazamoku <amadichiaza98@gmail.com>
2024-07-13 23:06:23 +0100

    Setup Redux. Implemented toggle theme.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
+import { useAppSelector } from "../app/hooks";
 import Navbar from "../components/Navbar";
 
 const Home: React.FC = () => {
+  const isDarkMode = useAppSelector((state) => state.theme.isDarkMode);
+
   return (
     <div className="flex h-screen w-full">
       <Navbar />
 
-      <main className="bg-dark-background h-screen w-full"></main>
+      <main
+        className={`${
+          isDarkMode ? "bg-dark-background" : "bg-light-background"
+        } h-screen w-full`}
+      ></main>
     </div>
   );
 };
